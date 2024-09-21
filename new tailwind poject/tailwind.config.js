92dbae414c91d6b0e81a8dc4b767d97c95c30388
Ibrahim Mondal <ibrahimmondal750@gmail.com>
2024-09-21 20:50:49 +0530

    add taliwine

---
+++ new tailwind poject/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        'buttan-bg':'#878cff',
+      }
+    },
+  },
+  plugins: [],
+}
+
